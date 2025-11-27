b4bf620ed6f31fbfb65f87f80454e797defd2a0e
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2025-11-27 15:23:56 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -1,12 +1,13 @@
 import sys
 
-script_name = sys.argv[0]   
-salary = float(sys.argv[1])  
+if(len(sys.argv))==2:
+  script_name = sys.argv[0]   
+  salary = float(sys.argv[1])  
+else:
+  salary =900
 
 bonus = salary * 0.10
 total_salary = salary + bonus
-
-
 print("Employee Salary:", salary)
 print("Bonus (10%):", bonus)
 print("Total Salary After Bonus:", total_salary)
